db5ecd3cb3805e170b6901268c741bed0d2f9f31
manuel <mendozac153@gmail.com>
2017-05-23 18:50:58 -0600

    Primer Commit

---
+++ src/proyectopoo/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ */
+package proyectopoo;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    protected Connection conexion;
+    //JDBC Driver y base de datos URL
+    private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private final String DB_URL = "jdbc:mysql://localhost:3306/evento";
+    
+    //Credenciales de la base de datos
+    private final String USER = "root";
+    private final String PASS = "";
+    
+    public static Statement sentencia;
+    
+    public Conexion() throws Exception{
+        try{
+            conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+            Class.forName(JDBC_DRIVER);
+        }catch(Exception e){
+            throw e;
+        }
+    }
+}
---
+++ src/proyectopoo/Principal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopoo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Manuel
+ */
+public class Principal {
+
+    void setTitle(String menu_Principal) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setLocationRelativeTo(Principal frm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
